974ab1e9d4a73defc6f7b074cc033b87149d1dec
sherry <sherrywahaj@gmail.com>
2021-01-14 16:41:04 -0600

    This is my first commit corejava sample programs

---
+++ practice/src/thisusage/argument/S2.java
@@ -0,0 +1,18 @@
+package thisusage.argument;
+
+public class S2 {
+
+    void m(S2 obj) {
+        System.out.println("method is invoked");
+    }
+    void p() {
+        System.out.println("p is started");
+        m(this);
+        System.out.println("p is completed");
+    }
+
+    public static void main(String[] args) {
+        S2 s1 = new S2();
+        s1.p();
+    }
+}
---
+++ practice/src/thisusage/thisreturn/A.java
@@ -0,0 +1,12 @@
+package thisusage.thisreturn;
+
+public class A {
+
+    A getA() {
+        return this;
+    }
+    void msg() {
+        System.out.println("Hello java");
+    }
+
+}
