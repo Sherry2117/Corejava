aeeda5c2c1906c6042ad25ce225a9e557f02a3cb
sherry <sherrywahaj@gmail.com>
2021-01-18 17:25:37 -0600

    polymorphism OverloadingCalculation3

--- practice/src/polymorphism/overriding/Bike.java
+++ practice/src/polymorphism/overriding/Bike.java
@@ -13,5 +13,8 @@ public class Bike extends Vehicle {
     public static void main(String[] args) {
         Bike obj = new Bike();
         obj.run();
+
+        Vehicle vehicle = new Vehicle();
+        vehicle.run();
     }
 }
