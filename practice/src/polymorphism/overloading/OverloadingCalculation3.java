c22a4c03aba984718d163ecbc6c9e04efad0de2a
sherry <sherrywahaj@gmail.com>
2021-01-18 16:28:40 -0600

    polymorphism overriding

---
+++ practice/src/polymorphism/overloading/OverloadingCalculation3.java
@@ -0,0 +1,12 @@
+package polymorphism.overloading;
+
+public class OverloadingCalculation3 {
+
+    void sum(int a, long b) {
+        System.out.println("a method invoked");
+    }
+
+    void sum(int a, long b) {
+        System.out.println("a method invoked");
+    }
+}
