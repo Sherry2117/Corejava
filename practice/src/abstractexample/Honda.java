55f4b1165bad921c203815bf95fe5355e7e76c21
sherry <sherrywahaj@gmail.com>
2021-01-23 21:15:35 -0600

    abstract initialized examples

---
+++ practice/src/abstractexample/Honda.java
@@ -0,0 +1,18 @@
+package abstractexample;
+
+import thisusage.constt.call.B;
+
+abstract class Bike {
+    abstract void run();
+}
+public class Honda extends Bike {
+    void run() {
+        System.out.println("running safely");
+    }
+
+    public static void main(String[] args) {
+        Bike obj = new Honda();
+        obj.run();
+    }
+}
+
