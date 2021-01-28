5815ff217eb9c79817ab2f5626473e763df93218
sherry <sherrywahaj@gmail.com>
2021-01-28 15:02:38 -0600

    package & private access modifier

---
+++ practice/src/accessmodifiers/privatepkg/Simple.java
@@ -0,0 +1,14 @@
+package accessmodifiers.privatepkg;
+
+//Private access modifier, when you removed the private will print but with private it will shows data has a private access
+class A{
+private int data=40;
+}
+
+public class Simple {
+
+    public static void main(String[] args) {
+        /*A a = new A();
+        System.out.println("A data==>" + a.data);*/
+    }
+}
