3ce195d1c48c9997b7e1880497ab9c87b4f5897e
sherry <sherrywahaj@gmail.com>
2021-01-28 22:40:13 -0600

    default access modifier

---
+++ practice/src/accessmodifiers/defaultpkg/mypack/B.java
@@ -0,0 +1,10 @@
+package accessmodifiers.defaultpkg.mypack;
+
+import accessmodifiers.defaultpkg.A;
+public class B {
+
+    public static void main(String[] args) {
+        /*A a = new A ();
+        a.msg();*/
+    }
+}
