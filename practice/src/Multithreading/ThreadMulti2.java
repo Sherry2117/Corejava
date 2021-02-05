d8b9176372975bb8748e9e4b2b113fe6999d2148
sherry <sherrywahaj@gmail.com>
2021-02-05 17:44:18 -0600

    Threads examples part 2

---
+++ practice/src/Multithreading/ThreadMulti2.java
@@ -0,0 +1,29 @@
+package Multithreading;
+
+public class ThreadMulti2 extends Thread{
+    @Override
+    public void run() {
+        super.run();
+        System.out.println("Thread is running");
+        for(int i = 0; i<5; i++) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.out.println(e);
+            }
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ThreadMulti2 thread1 = new ThreadMulti2();
+        ThreadMulti2 thread2 = new ThreadMulti2();
+        thread1.start();
+        Thread.sleep(2000);
+        thread2.run();
+
+    }
+
+}
+
